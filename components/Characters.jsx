8d5719041577c476fc22185d5f6927fceece7588
Manuperex <104181677+Manuperex@users.noreply.github.com>
2022-12-13 14:50:02 -0500

    primero commit

---
+++ components/Characters.jsx
@@ -0,0 +1,9 @@
+function Characters({character}) {
+    return (
+        <div className="text-center p-5">
+          <h3>{character.name}</h3>
+          <img className="img-fluid rounded-pill" src={character.image} />
+          <p>{character.origin.name}</p>
+        </div>)
+}
+export default Characters;
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import CharacterList from "../components/CharacterList";
+
+
+function App() {
+ return(
+  <div className="bg-dark text-white">
+    <h2 className="text-center display-1 py-4">Rick and Morty</h2>
+    <CharacterList/>
+  </div>
+
+)
+
+
+}
+
+
+
+
+export default App
